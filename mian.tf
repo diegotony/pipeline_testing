182cf6b1cc9489adb98f243908ee077b5848e535
Diego Castillo <diegotony1396@gmail.com>
2022-06-29 12:30:31 -0500

    Update mian.tf

--- mian.tf
+++ mian.tf
@@ -1,6 +1,6 @@
 module "minecraft_server" {
     source = "git::https://github.com/diegotony/fun_minecraft_server.git?ref=main"
     name = "minecraft-server"
-    description = "Just Survive"
+    description = "Just Survive" # optional
     tags = {"hey":"whats up"} # optional
 }
